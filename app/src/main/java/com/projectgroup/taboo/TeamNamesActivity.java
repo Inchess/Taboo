2b03ce27a400f986063e81b71b20615bac94f5ac
Tomasz Kubit <tomasz.kubit@performgroup.com>
2016-10-04 15:52:54 +0200

    Modify xml

--- app/src/main/java/com/projectgroup/taboo/TeamNamesActivity.java
+++ app/src/main/java/com/projectgroup/taboo/TeamNamesActivity.java
@@ -1,8 +1,18 @@
 package com.projectgroup.taboo;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
 /**
  * Created by tomasz.kubit on 2016-10-04.
  */
 
-public class TeamNamesActivity {
+public class TeamNamesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_names);
+    }
 }
